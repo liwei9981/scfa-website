074ea22159e677ae02626507d46293d25907f8fa
Li Wei <liwei9981@gmail.com>
2026-02-15 01:10:39 +0800

    Update news items with separate summaries and detailed formatted content

--- src/lib/data/news.ts
+++ src/lib/data/news.ts
@@ -87,6 +87,10 @@ export const NEWS_ITEMS: NewsItem[] = [
             zh: "2017年12月20日"
         },
         summary: {
+            en: "On 20th December 2017, the Delegation from Nanjing Zhongshan Mountain National Park paid a visit to SCFA, led by Mr Liu Zhenyu, Deputy Director of Zhongshan Mountain National Park.",
+            zh: "2017年12月20日上午，由南京市中山陵园管理局、南京市客运交通管理处组成的考察团一行8人拜访新中友协。双方就城市管理创新等领域进行了深入探讨。"
+        },
+        content: {
             en: "On 20th December 2017, the Delegation from Nanjing Zhongshan Mountain National Park paid a visit to SCFA, Mr Liu Zhenyu, Deputy Director of Zhongshan Mountain National Park led the Delegation, members of the Delegation included 7 officers from Jiaxing Municipal Government.\n\nSCFA President Prof Phua Kok Khoo, Honorary Secretary Mr Ng Yan Huay and committee member Mr Wang Shouzhi received the delegation. Mr Liu gave a brief introduction about the itinerary of the delegation and the purpose of the visit. In the meeting, both sides exchanged views and ideas to further strengthen cooperation in education, city government and technological research cooperation between SCFA and Nanjing.",
             zh: "2017年12月20日上午，由南京市中山陵园管理局、南京市客运交通管理处组成的考察团一行8人，在中山陵园管理局刘振宇副局长的带领下，前来新中友协交流考察。新中友协会长潘国驹教授、秘书长黄延辉先生以及董事王守志先生接待了考察团一行。\n\n潘教授对考察团一行表示欢迎。他表示，新中友协历年来不断加强与中国各省、直辖市及各大城市的交流沟通，南京有很好的生态环境、历史沉淀和教育资源，加强新加坡与南京市的合作联系很有必要。他表示，南京市可以借鉴新加坡现代化的管理经验，同时，新加坡也可以与南京众多大学和科研机构合作，在城市管理创新方面开展新的合作，促进两地对交通管理的科学研究。新中友协将择机拜访南京、上海等华东城市，探讨在教育、科技方面的友好合作。\n\n刘振宇副局长对新中友协的热情接待表示感谢，并介绍了此行的目的。他表示，希望通过与新加坡交通管理、市政管理相关的政府部门和企业交流，为缓解南京中山陵园景区交通管理、优化园区电子收费系统积累经验。同时，将积极向市政府反馈友协提出的各种建议，期待与新中友协进一步合作。访问结束后，宾主双方还互赠了纪念品，并合影留念。"
         },
@@ -135,12 +139,12 @@ export const NEWS_ITEMS: NewsItem[] = [
             zh: "2017年10月30日"
         },
         summary: {
-            en: "On 30th October 2017, a 3-person delegation from Shanghai International Culture Association, led by Ms Sun Hui, Deputy Director of General Office, visited SCFA. On behalf of SCFA, Mr Loh Weng Kee and Mr Wang Shouzhi received the delegation.",
-            zh: "2017年10月30日上午，上海市对外文化交流协会代表团一行三人，在办公室副主任孙晖的带领下，拜访新中友协。新中友协外事交际组主任罗荣基与副主任王守志代表接待了代表团一行。"
+            en: "A 3-person delegation from Shanghai International Culture Association visited SCFA to discuss future cooperation in arts and cultural sectors.",
+            zh: "2017年10月30日上午，上海市对外文化交流协会代表团一行三人拜访新中友协。双方就文化艺术等方面的国际交流探讨了合作的可能性。"
         },
         content: {
-            en: "On 30th October 2017, a 3-person delegation from Shanghai International Culture Association, led by Ms Sun Hui, Deputy Director of General Office, visited SCFA. On behalf of SCFA, Mr Loh Weng Kee and Mr Wang Shouzhi (Head and Deputy Head of External Relations & Social Activities Group) received the delegation. During the meeting, both sides exchanged ideas and discussed on the future cooperation in arts and cultural sector. Ms Sun said, Singapore and Shanghai is very similar, Shanghai can learn a lot from Singapore's experience. Meanwhile, she also invited SCFA to visit Shanghai in future.",
-            zh: "2017年10月30日上午，上海市对外文化交流协会代表团一行三人，在办公室副主任孙晖的带领下，拜访新中友协。新中友协外事交际组主任罗荣基与副主任王守志代表接待了代表团一行。上海市对外文化交流协会自1986年成立以来，积极且广泛推动文化艺术、科学技术、经济金融、人文社会、教育体育等各领域的国际交流活动。本次拜访，双方就文化艺术如书画、戏剧、音乐等方面，探讨合作、交流的可能。孙副主任表示，新加坡与上海有许多相近之处，在中西文化交流方面有许多值得借鉴的地方，并借此机会邀请我会拜访上海。"
+            en: "On 30th October 2017, a 3-person delegation from Shanghai International Culture Association, led by Ms Sun Hui, Deputy Director of General Office, visited SCFA. On behalf of SCFA, Mr Loh Weng Kee and Mr Wang Shouzhi (Head and Deputy Head of External Relations & Social Activities Group) received the delegation.\n\nDuring the meeting, both sides exchanged ideas and discussed on the future cooperation in arts and cultural sector. Ms Sun said, Singapore and Shanghai is very similar, Shanghai can learn a lot from Singapore's experience. Meanwhile, she also invited SCFA to visit Shanghai in future.",
+            zh: "2017年10月30日上午，上海市对外文化交流协会代表团一行三人，在办公室副主任孙晖的带领下，拜访新中友协。新中友协外事交际组主任罗荣基与副主任王守志代表接待了代表团一行。\n\n上海市对外文化交流协会自1986年成立以来，积极且广泛推动文化艺术、科学技术、经济金融、人文社会、教育体育等各领域的国际交流活动。本次拜访，双方就文化艺术如书画、戏剧、音乐等方面，探讨合作、交流的可能。\n\n孙副主任表示，新加坡与上海有许多相近之处，在中西文化交流方面有许多值得借鉴的地方，并借此机会邀请我会拜访上海。"
         },
         image: "/news/shanghai-culture-visit.jpg"
     }
